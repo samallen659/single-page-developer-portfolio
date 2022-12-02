f95bad28616a47ad02416713a285cb642d3f8384
Sam Allen <samallen659@gmail.com>
2022-12-02 18:18:22 +0000

    tests written for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { githubIcon } from '../assets/images/icon-github.svg';
+import { frontendMentorIcon } from '../assets/images/icon-frontend-mentor.svg';
+import { linkedinIcon } from '../assets/images/icon-linkedin.svg';
+import { twitterIcon } from '../assets/images/icon-twitter.svg';
 
 const Navbar = () => {
-	return <></>;
+	return (
+		<nav data-testid='navbar' className='flex justify-between'>
+			<h4>adamkeyes</h4>
+			<div data-testid='links'>
+				<a href='https://github.com'>
+					<img src={githubIcon} alt='github' />
+				</a>
+				<a href='https://frontendmentor.com'>
+					<img src={frontendMentorIcon} alt='frontendmentor' />
+				</a>
+				<a href='https://linkedin.com'>
+					<img src={linkedinIcon} alt='linkedin' />
+				</a>
+				<a href='https://twitter.com'>
+					<img src={twitterIcon} alt='twitter' />
+				</a>
+			</div>
+		</nav>
+	);
 };
 
 export { Navbar };
--- src/components/Navbar.test.jsx
+++ src/components/Navbar.test.jsx
@@ -2,9 +2,27 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { Navbar } from './Navbar';
 
-describe('Navvbar', () => {
-	test('renders a nav element', () => {
+describe('Navbar', () => {
+	const renderLink = (text) => {
+		it(`renders a link for ${text}`, () => {
+			render(<Navbar />);
+			expect(screen.getByAltText(text)).not.toBeUndefined();
+		});
+	};
+	it('renders a nav element', () => {
 		render(<Navbar />);
 		expect(screen.getAllByTestId('navbar')).not.toBeUndefined();
 	});
+	it('renders the heading', () => {
+		render(<Navbar />);
+		expect(screen.getAllByRole('heading')).not.toBeUndefined();
+	});
+	it('renders a div with data-testid links for the links', () => {
+		render(<Navbar />);
+		expect(screen.getAllByTestId('links')).not.toBeUndefined();
+	});
+	renderLink('github');
+	renderLink('frontendmentor');
+	renderLink('linkedin');
+	renderLink('twitter');
 });

daec1591320e64d8b889ade2b59cb6018eb89936
Sam Allen <samallen659@gmail.com>
2022-12-11 18:07:59 +0000

    Setup project data in its own file

---
+++ src/projectData.js
@@ -0,0 +1,51 @@
+import thumbnailLarge1 from './assets/images/thumbnail-project-1-large.webp';
+import thumbnailSmall1 from './assets/images/thumbnail-project-1-small.webp';
+import thumbnailLarge2 from './assets/images/thumbnail-project-2-large.webp';
+import thumbnailSmall2 from './assets/images/thumbnail-project-2-small.webp';
+import thumbnailLarge3 from './assets/images/thumbnail-project-3-large.webp';
+import thumbnailSmall3 from './assets/images/thumbnail-project-3-small.webp';
+import thumbnailLarge4 from './assets/images/thumbnail-project-4-large.webp';
+import thumbnailSmall4 from './assets/images/thumbnail-project-4-small.webp';
+import thumbnailLarge5 from './assets/images/thumbnail-project-5-large.webp';
+import thumbnailSmall5 from './assets/images/thumbnail-project-5-small.webp';
+import thumbnailLarge6 from './assets/images/thumbnail-project-6-large.webp';
+import thumbnailSmall6 from './assets/images/thumbnail-project-6-small.webp';
+
+export const projectData = [
+	{
+		title: 'Design Portfolio',
+		technologies: ['html', 'css'],
+		thumbnailLarge: thumbnailLarge1,
+		thumbnailSmall: thumbnailSmall1,
+	},
+	{
+		title: 'E-Learning landing page',
+		technologies: ['html', 'css'],
+		thumbnailLarge: thumbnailLarge2,
+		thumbnailSmall: thumbnailSmall2,
+	},
+	{
+		title: 'Todo Web App',
+		technologies: ['html', 'css', 'javascript'],
+		thumbnailLarge: thumbnailLarge3,
+		thumbnailSmall: thumbnailSmall3,
+	},
+	{
+		title: 'Entertainment Web App',
+		technologies: ['html', 'css', 'javascript'],
+		thumbnailLarge: thumbnailLarge4,
+		thumbnailSmall: thumbnailSmall4,
+	},
+	{
+		title: 'Memory Game',
+		technologies: ['html', 'css', 'javascript'],
+		thumbnailLarge: thumbnailLarge5,
+		thumbnailSmall: thumbnailSmall5,
+	},
+	{
+		title: 'Art Gallery Showcase',
+		technologies: ['html', 'css', 'javascript'],
+		thumbnailLarge: thumbnailLarge6,
+		thumbnailSmall: thumbnailSmall6,
+	},
+];

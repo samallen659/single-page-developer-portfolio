851180c2838fbdaebe3223d25dcd947f83ede742
Sam Allen <samallen659@gmail.com>
2022-12-11 18:52:36 +0000

    Added text color change on hover and amended href settings

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Button = ({ text, href }) => {
 	return (
-		<a data-testid='button' href={href} className='text-white underline decoration-secondary underline-offset-8 md:text-lg'>
+		<a
+			data-testid='button'
+			href={href ? href : '#'}
+			className='text-white underline decoration-secondary underline-offset-8 md:text-lg hover:text-secondary'>
 			{text.toUpperCase()}
 		</a>
 	);
